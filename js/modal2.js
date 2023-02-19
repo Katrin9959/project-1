11eed2b674eee8162f7600d7bc3bc45d64e6a3ab
Katrin9959 <122272747+Katrin9959@users.noreply.github.com>
2023-02-19 15:15:31 +0100

    12

--- js/modal2.js
+++ js/modal2.js
@@ -1,17 +1,18 @@
 
 
 (() => {
-    const refs = {
-      openModal2Btn: document.querySelector("[data-modal2-open]"),
-      closeModal2Btn: document.querySelector("[data-modal2-close]"),
+    const refs2 = {
+      openModa2lBtn: document.querySelector("[data-modal2-open]"),
+      closeModa2lBtn: document.querySelector("[data-modal2-close]"),
       modal2: document.querySelector("[data-modal2]"),
     };
   
-    refs.openModal2Btn.addEventListener("click", toggleModal2);
-    refs.closeModal2Btn.addEventListener("click", toggleModal2);
+    refs2.openModa2lBtn.addEventListener("click", toggleModal2);
+    refs2.closeModa2lBtn.addEventListener("click", toggleModal2);
   
     function toggleModal2() {
-      refs.modal2.classList.toggle("is-hidden2");
+      console.log(refs2);
+      refs2.modal2.classList.toggle("is-hidden2");
     }
     
   })();
